afa8752b0b2d21107b9c7a73bbdabf590ae7305d
wtdarsey <wtaylordarsey@icloud.com>
2025-11-17 11:02:25 +0000

    Added PY file
    to practice uploading files

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print("I am very excited to learn!")
+x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+
+print('Finish');
+'''conditional loop below'''
+n = 5
+while n > 0 :
+    print(n)
+    n = n - 1
+print('Blast off!');
